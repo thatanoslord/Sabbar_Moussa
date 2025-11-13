051862dd9fe5d6385e053f43b2bcde1d912d4c48
Your thatanoslord <sabbarmoussa@gmail.com>
2025-11-13 15:53:41 +0100

    Get current scroll position

--- scripts/about.js
+++ scripts/about.js
@@ -7,3 +7,45 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("navbar-expanded");
   }
 });
+
+//Nav link active state on scroll
+
+const Sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  // Get current scroll position
+  const scrollPosition =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  const isAtBottom =
+    window.innerHeight + scrollPosition >=
+    document.documentElement.scrollHeight - 50;
+
+  if (isAtBottom) {
+    const lastSection = Sections[Sections.length - 1];
+    current = lastSection.getAttribute("id");
+  } else {
+    Sections.forEach((section) => {
+      const sectionTop = section.offsetTop - 150;
+      const sectionHeight = section.clientHeight;
+
+      if (
+        scrollPosition >= sectionTop &&
+        scrollPosition < sectionTop + sectionHeight
+      ) {
+        current = section.getAttribute("id");
+      }
+    });
+  }
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (current && link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
